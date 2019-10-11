ab4d3cf12f6c0f0653d0330b32faa24f3ef5e132
JoshGuarino <guar0638@fredonia.edu>
2019-10-11 13:52:06 -0400

    working from json directory

---
+++ api.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask_restful import Resource, Api, abort
+import os
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+pokemon_data = {}
+pokemon_index = {}
+pokemon_directory = os.listdir('json')
+for item in pokemon_directory:
+    with open('json/' + item , 'r') as file:
+        data = file.read()
+    pokemon = json.loads(data)
+    pokemon_data[pokemon['name']] = pokemon
+    pokemon_index[pokemon['number']] = pokemon['name']
+
+
+def check_pokemon_exists(pokemon):
+    if pokemon not in pokemon_data and pokemon not in pokemon_index:
+        abort(404, message="Pokemon {} doesn't exist".format(pokemon))
+
+
+class PokemonAll(Resource):
+    def get(self):
+        return pokemon_data
+
+class Pokemon(Resource):
+    def get(self, poke_ident): 
+        if poke_ident.isnumeric():
+            poke_num = int(poke_ident)
+            check_pokemon_exists(poke_num)
+            return pokemon_data[pokemon_index[poke_num]]
+        poke_ident = poke_ident.capitalize()
+        check_pokemon_exists(poke_ident)
+        return pokemon_data[poke_ident]
+
+api.add_resource(PokemonAll, '/pokemon')
+api.add_resource(Pokemon, '/pokemon/<string:poke_ident>')
+
+if __name__ == '__main__':
+    app.run(debug=True) 
+
+    
